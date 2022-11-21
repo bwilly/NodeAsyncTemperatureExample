32bee6027f0df0595ca1038ac2cde1aff3ce61fe
Brian Willy <midstarevolution@gmail.com>
2022-11-21 14:02:03 +0000

    dht test file now takes CLI args for pin and async/sync

--- test-dht-sensor.js
+++ test-dht-sensor.js
@@ -19,9 +19,21 @@ const dht = require("node-dht-sensor");
 
 // let tempt = dht.read(22, 18);
 
+const program = require('commander');
+
+program
+    .option('-p, --pin <gpio>', 'DHT22 Pin GPIO Number')
+    .option('-a, --async', 'Async sensor call.')
+    .option('-s, --sync', 'Synchronous sensor call.')
+    .option('-c, --callback', 'Callback sensor call.')
+    .parse(process.argv);
+
+let options = program.opts();
+let pin = options.pin;
+console.log("DHT22 on Pin " + pin);
 
 function readTemptCallback() {
-    dht.read(22, 18, function (err, temperature, humidity) {
+    dht.read(22, pin, function (err, temperature, humidity) {
         if (!err) {
             console.log(`temp (inside callback): ${temperature}°C, humidity: ${humidity}%`);
         } else {
@@ -31,14 +43,14 @@ function readTemptCallback() {
 }
 
 function readTempt() {
-    let sensorResult = dht.read(22, 18);
+    let sensorResult = dht.read(22, pin);
     let temperature = sensorResult.temperature;
     let humidity = sensorResult.humidity;
     console.log(`temp (synchronous): ${temperature}°C, humidity: ${humidity}%`);
 }
 
 async function readTemptAsync() {
-    let sensorResult = await dht.read(22, 18);
+    let sensorResult = await dht.read(22, pin);
     let temperature = sensorResult.temperature;
     let humidity = sensorResult.humidity;
     console.log(`temp (inside asyc): ${temperature}°C, humidity: ${humidity}%`);
@@ -46,8 +58,14 @@ async function readTemptAsync() {
 
 
 console.log("Starting.")
-readTemptCallback();
-readTempt();
-readTemptAsync();
+if (options.callback) {
+    readTemptCallback();
+}
+if (options.sync) {
+    readTempt();
+}
+if (options.async) {
+    readTemptAsync();
+}
 console.log("Stopping.")
 // console.log("Temperature via await's val: " + tempt);
